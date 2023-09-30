55046e06c40715b360b1af14c49bffba807190d0
Leonardo <leonardomarron235@gmail.com>
2023-09-29 23:42:32 -0600

    practica1

---
+++ app/src/main/java/mx/edu/isc/tesoem/mml/p1_7s21/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.mml.p1_7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
